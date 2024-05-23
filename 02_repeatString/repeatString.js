cb35546a02b442ee58ae25f3d011164232005e64
1223akash <139630240+1223akash@users.noreply.github.com>
2024-05-23 23:34:13 +0530

    Complete first seven exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, frequency) {
+    let result = "";
+    if (frequency < 0) return "ERROR";
+    for (let i = 1; i <= frequency; i++) {
+        result += str;
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (start < 0 || typeof start === "string" || Array.isArray(start)) return "ERROR";
+    if (end < 0 || typeof end === "string" || Array.isArray(end)) return "ERROR";
+    if (start > end) {
+        [start, end] = [end, start];  
+    }
+    let sum = 0;
+    for (let i = start; i <= end; i++)
+        sum += i;
+    return sum;
 };
 
 // Do not edit below this line
